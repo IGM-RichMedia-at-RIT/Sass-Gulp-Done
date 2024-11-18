15a4eab9399892e7a37be8590ee82f2f0651ea88
Austin Willoughby <arwigm@rit.edu>
2024-11-18 13:12:29 -0500

    Package updates

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,25 +1,18 @@
-const example1 = (req, res) => {
-  return res.render('example1');
-};
+const example1 = (req, res) => res.render('example1');
 
-const example2 = (req, res) => {
-  return res.render('example2');
-};
+const example2 = (req, res) => res.render('example2');
 
 const example3 = (req, res) => {
   console.log('test');
   return res.render('example3');
 };
 
-const getSongs = (req, res) => {
-  return res.json([
-    { artist: 'c418', title: 'Minecraft' },
-    { artist: 'Chris Tilton', title: 'SimCity, 2019' },
-  ]);
-};
+const getSongs = (req, res) => res.json([
+  { artist: 'c418', title: 'Minecraft' },
+  { artist: 'Chris Tilton', title: 'SimCity, 2019' },
+]);
 
 module.exports.example1 = example1;
 module.exports.example2 = example2;
 module.exports.example3 = example3;
 module.exports.getSongs = getSongs;
-
