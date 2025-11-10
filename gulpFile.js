0c22fe08773a48d08f906fcb304910549c5428e2
Austin Willoughby <arwigm@rit.edu>
2025-11-10 13:31:25 -0500

    Package updates

--- gulpFile.js
+++ gulpFile.js
@@ -14,17 +14,18 @@
   by a different developer they all have their own syntax and
   documentation that you'll need to follow. Below we import a number
   of gulp-friendly versions of libraries we have used before such
-  as eslint, nodemon, webpack, etc. We also import the root gulp
-  library, and we import gulp-sass which will build our scss into
-  css. Note that we give it the dart-sass library to use for doing
-  the building.
+  as eslint, webpack, etc. We also import the root gulp library, 
+  and we import gulp-sass which will build our scss into css. Note 
+  that we give it the sass library to use for doing the actual building.
 */
 const gulp = require('gulp');
+const { spawn } = require('child_process');
 const sass = require('gulp-sass')(require('sass'));
 const webpack = require('webpack-stream');
-const nodemon = require('gulp-nodemon');
 const eslint = require('gulp-eslint-new');
 const webpackConfig = require('./webpack.config.js');
+
+let nodeProcess = null;
 
 /*Here is our first gulp task. Gulp tasks are defined as functions
   and have one requirement. They must take in a callback function
@@ -98,6 +99,24 @@ const lintTask = (done) => {
 const build = gulp.parallel(sassTask, jsTask, lintTask);
 const herokuBuild = gulp.parallel(sassTask, jsTask);
 
+/*There used to be a maintained gulp-nodemon package, but it has not
+  been updated in a number of years. While base nodemon still works,
+  to have something work elegantly with gulp we have to instead make
+  our own basic restarter like nodemon. Fortunately with the node
+  child_process library we can spin up our server in a thread like
+  nodemon would.
+
+  We will pair this with gulp.watch() and our lint task down in the
+  watch function below.
+*/
+const serverTask = (done) => {
+  if(nodeProcess) {
+    nodeProcess.kill();
+  }
+  nodeProcess = spawn('node', ['./server/app.js'], { stdio: 'inherit' });
+  done();
+}
+
 /*This watch task below is doing quite a lot. The gulp.watch()
   function takes in a single file, folder, pattern, or array
   of files/folders/patterns and observes them. When any of the
@@ -110,31 +129,26 @@ const herokuBuild = gulp.parallel(sassTask, jsTask);
   The second watch statement says any time a .jsx or .js file in
   the client folder changes, the jsTask should be run.
 
-  We will also use our watch task to run nodemon. The gulp-nodemon
-  library takes in a few options. The script option defines which
-  file should be run on restart (our app.js file). The tasks array
-  defines which tasks to run before restarting. Note that these tasks
-  need to be exported from our gulpFile to work properly. In this 
-  case, when our server code is about to restart, we will lint it first.
+  Finally, we will tell gulp to watch our server folder for changes.
+  When they happen, it will trigger the series of tasks described.
+  In this case, it will run our lint task, and then our server task.
 
-  The watch option tells nodemon which folder to watch for changes.
-  Finally we also give it our done callback so that it properly tells
-  gulp when it has stopped watching our code.
+  For all of these tasks, we say { ignoreInitial: false } because by
+  default, gulp will not run the task on startup. If we didn't set this,
+  our server would only start when our code changed. Our scss wouldn't
+  build until we edited it. Our react code wouldn't bundle until we
+  changed it.
 */
 const watch = (done) => {
-    gulp.watch('./scss', sassTask);
-    gulp.watch(['./client/*.js', './client/*.jsx'], jsTask);
-    nodemon({ 
-        script: './server/app.js',
-        tasks: ['lintTask'],
-        watch: ['./server'],
-        done: done
-    });
+    gulp.watch('./scss', { ignoreInitial: false }, sassTask);
+    gulp.watch(['./client/*.js', './client/*.jsx'], { ignoreInitial: false }, jsTask);
+    gulp.watch('./server/**/*.js', { ignoreInitial: false }, gulp.series(lintTask, serverTask));
+
+    done();
 }
 
 /*From our gulpFile, we want to export any tasks that we want
-  to be able to call from our package.json or that we need to
-  be called by packages like gulp-nodemon. In this case we can
+  to be able to call from our package.json. In this case we can
   simply export all of them.
 
   To call a gulp task from a package.json script, we simply
